a3819c5e2908a2965f92fd1f4cd86398c27e2e6f
Yaroslav Liakh <liakh_ya@mail.ru>
2016-03-11 00:25:09 +0200

    js first 3 variants

--- JS_FE4/hw1/js/login.js
+++ JS_FE4/hw1/js/login.js
@@ -4,15 +4,15 @@
 "use strict"
 var arrName = [];
 for(var i=0; i < 5; i++){
-    arrName[i] = prompt("Введите имя " + (i+1) + " пользователя");
+    arrName[i] = prompt("Enter the name " + (i+1) + " members");
 }
-var login = prompt("Введите логин");
+var login = prompt("Enter the name of members");
 for (i=0; i<arrName.length ; i++){
     if(login == arrName[i]){
         var successfulLogin = arrName[i];
     }
 }
- if ((successfulLogin != null)&& (successfulLogin != '')){
+ if ((successfulLogin !== null)&& (successfulLogin !== '')){
      alert(successfulLogin + ", Вы успешно вошли");
  }else{
      alert("ЭРОР404!");
--- JS_FE4/hw1/js/powMy.js
+++ JS_FE4/hw1/js/powMy.js
@@ -3,11 +3,11 @@
  */
 "use strict"
 
-var m = prompt("указанное число:");
-var n = prompt("указанная степень:");
+var m = prompt("указанное число: ");
+var n = prompt("указанная степень: ");
 
 /*recursion*/
-function powMy(a ,b){
+function powMyV1(a ,b){
     if (b < 0){
         console.log("ERROR");
     }
@@ -22,5 +22,43 @@ function powMy(a ,b){
     }
 }
 
+/*Exponential*/
+function powMyV2(a ,b){
+    if (b < 0){
+        console.log("ERROR");
+    }
+    else if (b == 0) {
+        return 1;
+    }
+    else if (b == 1) {
+        return a;
+    }
+    else {
+        var result = Math.exp(a*(Math.log(b)));
+        return result;
+    }
+}
+
+/*for (most simple)*/
+function powMyV3(a ,b){
+    var result = 1;
+    if (b < 0){
+        console.log("ERROR");
+    }
+    else if (b == 0) {
+        return 1;
+    }
+    else if (b == 1) {
+        return a;
+    }
+    else {
+        for (var i = 0; i < b; i++) {
+            var result = result*a;
+        }
+        return result;
+    }
+}
 /*OUTPUT*/
-console.log(powMy(m,n));
+console.log(powMyV1(m,n));
+console.log(powMyV2(m,n));
+console.log(powMyV3(m,n));
