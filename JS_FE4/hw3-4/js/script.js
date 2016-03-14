2e8a9262cd0a4a1e8f86c12761154bb739311979
Yaroslav Liakh <liakh_ya@mail.ru>
2016-03-14 20:10:18 +0200

    Осталось настроить стили :)

--- JS_FE4/hw3-4/js/script.js
+++ JS_FE4/hw3-4/js/script.js
@@ -3,52 +3,57 @@
  */
 "use strict"
 var mainObj = {
-    createWrapper : function(){
+    createWrapper: function () {
         var wrapper = document.createElement('div');
         wrapper.classList.add('wrapper');
         document.body.appendChild(wrapper)
     },
-    createHeader : function(){
+    createHeader: function () {
         var header = document.createElement('p');
         header.innerHTML = 'Тест по программированию';
         header.classList.add('center-header');
         var wrapper = document.querySelector('.wrapper');
         wrapper.insertBefore(header, null);
     },
-    questionsAndAnswers : function(){
+    questionsAndAnswers: function () {
         var qAndA = [];
         /* Здесь будет происходить наполнение массива
-        *q AndA*/
+         *q AndA*/
 
         /*вопрос-ответ №1*/
         var qa1 = {
-            question : 'Вопрос №1',
-            answers  : ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
+            question: 'Вопрос №1',
+            answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
         }
         qAndA.push(qa1);
+
         /*вопрос-ответ №2*/
         var qa2 = {
-            question : 'Вопрос №2',
-            answers  : ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
+            question: 'Вопрос №2',
+            answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
         }
         qAndA.push(qa2);
+
         /*вопрос-ответ №3*/
         var qa3 = {
-            question : 'Вопрос №3',
-            answers  : ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
+            question: 'Вопрос №3',
+            answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
         }
         qAndA.push(qa3);
         console.log(qAndA);
+
         /*создание формы и заполнение ее*/
         var form = document.createElement('form');
         form.classList.add('form-qa');
         var wrapper = document.querySelector('.wrapper');
         wrapper.appendChild(form);
-        for (var i=0; i<qAndA.length; i++){
+        for (var i = 0; i < qAndA.length; i++) {
+
             /* Создание <p> */
             var pQuestion = document.createElement('p');
             pQuestion.classList.add('p-questions');
             form.appendChild(pQuestion);
+
             /* Создание <label> в <p> */
             var labelQuestion = document.createElement('label');
             labelQuestion.classList.add('label-questions')
@@ -56,16 +61,24 @@ var mainObj = {
             labelQuestion.innerHTML = counter + ". " + qAndA[i].question;
             pQuestion.appendChild(labelQuestion);
             /* цикл на вывод ответов*/
-            for (var j=0; j<3 ; j++){
+
+            for (var j = 0; j < 3; j++) {
+
+                /* создание p, в который вложены checkbox и label */
+                var pAnswers = document.createElement('p');
+                pAnswers.classList.add('p-answers');
+                form.appendChild(pAnswers);
+
+                /* создание и вывод Checkbox */
+                var checkboxAnswers = document.createElement("INPUT");
+                checkboxAnswers.setAttribute("type", "checkbox");
+                pAnswers.appendChild(checkboxAnswers);
+
+                /* создание и вывод label */
                 var labelAnswers = document.createElement('label');
                 labelAnswers.classList.add('label-answers');
-                pQuestion.appendChild(labelAnswers);
-                /* создание и вывод Checkbox ВОТ ТУТ ПРОБЛЕМА! ВНИМАТЕЛЬНО ВСЕ ПРОВЕРИТЬ*/
-                //var inputCheckbox = document.createElement('input');
-                //inputCheckbox.classList.add('checkbox-answers');
-                //inputCheckbox.appendChild(labelAnswers);
-                //inputCheckbox.setAttribute('type', 'checkbox');
-                //inputCheckbox.innerHTML = qAndA[i].answers[j];
+                pAnswers.appendChild(labelAnswers);
+                labelAnswers.innerHTML = qAndA[i].answers[j];
             }
         }
     }
