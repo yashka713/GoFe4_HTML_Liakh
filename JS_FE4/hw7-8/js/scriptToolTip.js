d3de622ae7dbbf653b33168efff942aaa004eba5
Yaroslav Liakh <liakh_ya@mail.ru>
2016-03-30 22:47:05 +0300

    на сдачу

--- JS_FE4/hw7-8/js/scriptToolTip.js
+++ JS_FE4/hw7-8/js/scriptToolTip.js
@@ -1,5 +1,19 @@
 'use strict'
+
 $(document).ready(function () {
-
-
+    $('label + input').hover(
+        function(){
+            $(this).parent().append('<span>' + $(this).attr('title') + '</span>');
+        },function(){
+            $('span').remove();
+    });
+    $('.btn_helper').on('click', function(){
+        $('span').remove();
+        $('label + input').each(function() {
+            $(this).parent().append('<span>' + $(this).attr('title') + '</span>');
+        });
+        setTimeout(function(){
+            $('span').remove();
+        }, 4000);
+    })
 });
