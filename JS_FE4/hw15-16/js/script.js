81a6380960dfe878d8336213c7fa9a56cdb7516c
Yaroslav Liakh <liakh_ya@mail.ru>
2016-05-02 22:03:13 +0300

    work API

--- JS_FE4/hw15-16/js/script.js
+++ JS_FE4/hw15-16/js/script.js
@@ -2,24 +2,8 @@
 var obj;
 var value;
 
-
-$(document).ready(function () {
-
-    $('.wrapper-submit').on('click'|| 'keypress', function(){
-    value = $('.wrapper-text').val();
-        console.log(value);
-
-        $.ajax({
-        url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg' +
-            '&rsz=large' + '&q=' + value + '&callback=GoogleCallback&context=?',
-        dataType: 'jsonp'
-    });
-    })
-});
-
-function GoogleCallback (jqueryObj, data) {
+function GoogleCallback(jqueryObj, data) {
     obj = data.results;
-    console.log('value', obj);
 
     var header = $('#base-template').html();
     var template = tmpl(header, obj);
@@ -27,3 +11,22 @@ function GoogleCallback (jqueryObj, data) {
     $('.wrapper-answer').append(template);
 
 }
+
+$(document).ready(function () {
+
+    function Sender() {
+        console.log('123');
+        $('.answers').remove();
+        value = $('.wrapper-text').val();
+        $.ajax({
+            url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg' +
+            '&rsz=large' + '&q=' + value + '&callback=GoogleCallback&context=?',
+            dataType: 'jsonp'
+        });
+    }
+
+    document.onkeydown = function(e){
+        if (e.keyCode==13) Sender();
+    };
+    $('.wrapper-submit').on('click', Sender);
+});
