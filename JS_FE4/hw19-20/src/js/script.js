0a4905ffeaab30ba9876bffe565b4f07e0536120
Yaroslav Liakh <liakh_ya@mail.ru>
2016-05-09 21:53:14 +0300

    first commit

---
+++ JS_FE4/hw19-20/src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('.carousel-list').myCarousell();
+
+    var header = $('#base-template').html();
+    var template = tmpl(header, base);
+    $('.wrapper').append(template);
+
+});
