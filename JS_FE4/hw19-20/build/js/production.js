f3416d7c250eddbb54abcfbd6487a3ae25ae12af
Yaroslav Liakh <liakh_ya@mail.ru>
2016-05-16 21:55:33 +0300

    no message

---
+++ JS_FE4/hw19-20/build/js/production.js
@@ -0,0 +1,1 @@
+$(function(){$(function(){$(".jcarousel").jcarousel({}),$(".jcarousel-prev").click(function(){$(".jcarousel").jcarousel("scroll","-=1")}),$(".jcarousel-next").click(function(){$(".jcarousel").jcarousel("scroll","+=1")})})});
---
+++ JS_FE4/hw19-20/src/js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(function() {
+        $('.jcarousel').jcarousel({
+            // Core configuration goes here
+        });
+
+        $('.jcarousel-prev').click(function() {
+            $('.jcarousel').jcarousel('scroll', '-=1');
+        });
+
+        $('.jcarousel-next').click(function() {
+            $('.jcarousel').jcarousel('scroll', '+=1');
+        });
+    });
+});
