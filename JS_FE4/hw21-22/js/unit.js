5721642fc4d7c59b4dd3f0a6204b1f9dcf8389ce
Yaroslav Liakh <liakh_ya@mail.ru>
2016-05-19 21:12:14 +0300

    no message

--- JS_FE4/hw21-22/js/unit.js
+++ JS_FE4/hw21-22/js/unit.js
@@ -1,40 +1,45 @@
 var unit = {
-    GCD: function (a,b) {
-        try{
-            var r;          //остаток от деления a на b
-            var q;          //частное от целочисленного деления a на b ; a = b*q + r
-            var m1 = 1;     //a = m1 * a + n1 * b
-            var n1 = 0;
-            var m2 = 0;     //b = m2 * a + n2 * b
-            var n2 = 1;
-            var t;
-            while (b!=0)
-            {
-                r = a%b;
-                q = (a-r)/b;
-                a = b;
-                b = r;
-                t = m1 - q * m2;
-                m1 = m2;
-                m2 = t;
-                t = n1 - q * n2;
-                n1 = n2;
-                n2 = t;
-            }
-            var str = 'Наибольший общий делитель чисел '+ a + ' и ' + b + ' равен '+ a +';';
-            if (a<0)  a = '('+a+')';
-            if (b<0)  b = '('+b+')';
-            if (m1<0)  m1 = '('+m1+')';
-            if (n1<0)  n1 = '('+n1+')';
-            str = str + a + ' = '+ m1 + ' * ' + a + ' + ' + n1 + ' * ' + b;
-            return console.log(a);
+    GCD: function (a, b) {
+        try {
+                a = Math.abs(a);
+                b = Math.abs(b);
+                var r;          //остаток от деления a на b
+                var q;          //частное от целочисленного деления a на b ; a = b*q + r
+                var m1 = 1;     //a = m1 * a + n1 * b
+                var n1 = 0;
+                var m2 = 0;     //b = m2 * a + n2 * b
+                var n2 = 1;
+                var t;
+                while (b != 0) {
+                    r = a % b;
+                    q = (a - r) / b;
+                    a = b;
+                    b = r;
+                    t = m1 - q * m2;
+                    m1 = m2;
+                    m2 = t;
+                    t = n1 - q * n2;
+                    n1 = n2;
+                    n2 = t;
+                }
+                var str = a;
+                if (a < 0)  a = '(' + a + ')';
+                if (b < 0)  b = '(' + b + ')';
+                if (m1 < 0)  m1 = '(' + m1 + ')';
+                if (n1 < 0)  n1 = '(' + n1 + ')';
+                return a;
         }
-        catch (e){
+        catch (e) {
             console.log("!something wrong!")
         }
     }
 };
-unit.GCD(-5,0);
+
+        console.log(unit.GCD(10, 25));
 
 //разрешение на вызов файла в nodeJs
-module.exports = unit;
+try {
+    module.exports = unit;
+} catch (e) {
+    console.log("!something wrong with 'module.exports'")
+}
--- JS_FE4/hw21-22/spec/testSpec.js
+++ JS_FE4/hw21-22/spec/testSpec.js
@@ -2,17 +2,41 @@
  * Created by Ярик on 19.05.2016.
  */
 // найти файл и подключить его к тестированию
-var unit = require('../js/unit.js');
-
+var obj = require('../js/unit.js');
 describe("Проверка самого алгоритма НОД(базовые требования)", function() {
 
-    it("НОД(0, n) = n; НОД(m, 0) = m; НОД(m, m) = m", function() {
-        //подготовка (prepare)
-
-        //действие (act)
-
-        //результат(assert)
-
-        expect(true).toBe(true);
+    it("НОД(m, n)", function() {
+        var result1;
+        result1 = obj.GCD(18,222);
+        expect(result1).toBe(6);
     });
+    it("НОД(0, n) = n; НОД(m, 0) = m;НОД(m, m) = m", function() {
+        var result1, result2, result3;
+        result1 = obj.GCD(0,5);
+        result2 = obj.GCD(5,0);
+        result3 = obj.GCD(5,5);
+        expect(result1).toBe(5);
+        expect(result2).toBe(5);
+        expect(result3).toBe(5);
+    });
+    it("НОД(1, n) = 1; НОД(m, 1) = 1;", function() {
+        //подготовка (prepare)
+        var result1, result2;
+        //действие (act)
+        result1 = obj.GCD(1,5);
+        result2 = obj.GCD(5,1);
+        //результат(assert)
+        expect(result1).toBe(1);
+        expect(result2).toBe(1);
+    });
+});
+
+describe("Проверка 'уловок'", function() {
+
+    it("НОД(m, n)", function() {
+        var result1;
+        result1 = obj.GCD(18,222);
+        expect(result1).toBe(6);
+    });
+
 });
